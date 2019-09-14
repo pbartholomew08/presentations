a9f64f239a7a43383e2480b84107bd8fcb87f4e6
Paul Bartholomew <ptb0890@gmail.com>
2019-09-14 16:13:56 +0100

    Setting up dir-locals for presentations
    This ensures that the font size of code blocks is reasonable for
    beamer

---
+++ .dir-locals.el
@@ -0,0 +1,10 @@
+;;;;
+;;
+
+((nil . ((org-latex-listings-options . (("basicstyle" "\\ttfamily\\tiny")
+					("keywordstyle" "\\color{blue}\\bfseries")
+					("commentstyle" "\\color{red}")
+					("stringstyle" "\\color{magenta}")
+					("frame" "single")
+					("showstringspaces" "false")
+					("breaklines" "true"))))))
